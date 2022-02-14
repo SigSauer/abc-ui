dc5a26fbd35ca0009b2ed340ea7559c518f21d1e
dplaskii <daniel.sigsauer.195@gmail.com>
2022-02-14 04:52:03 +0200

    heroku deployment
    - implemented server.js to heroku deployment;
    - updated Home.vue according to new design;

--- src/views/default/Home.vue
+++ src/views/default/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <div class="jumbotron">
-      <h1 class="display-4">ABC | ОКБС</h1>
-      <p><a href="https://github.com/SigSauer">by SigSauer</a></p>
-      <p class="lead">Accounting of Bank Clients | Облік клієнтів банківскої системи</p>
+      <h1 class="display-4">CRM Banking | CRM Банкінг</h1>
+      <p><a href="https://github.com/SigSauer">by Daniel Plaksii</a></p>
+      <p class="lead">Customer relation management "Banking" | Система керування клієнтами "Банкінг" </p>
       <hr class="my-4">
       <p>Даний програмний продукт призначений для обліку клієнтів банківської системи.
         Використання даного програмного забезпечення має здійснюватись у відповідності до Закону України
@@ -11,7 +11,7 @@
       <p v-if="!currentUser">Для початку роботи з системою,
         <router-link to="/login">авторизуйтесь</router-link>
       </p>
-
+      <p class="lead">DEVELOPMENT MODE</p>
     </div>
   </div>
 </template>
